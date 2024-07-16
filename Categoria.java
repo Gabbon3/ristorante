08cc7848a421c2815202d3b3c83395b1b581d554
alessandrocosta7 <alessandro.costa@edu.itspiemonte.it>
2024-07-16 10:32:40 +0200

    import array list

--- Categoria.java
+++ Categoria.java
@@ -1,12 +1,14 @@
 
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Categoria {
     public String Id;
     public String Nome;//Nome piatto es: "Prosciutto crudo e melone"
     public List<Piatto> Piatti;//categoria piatti (A= antipasti, P = primi, S = secondi, C = contorni, D = dolci)
 
-    public Categoria(String Id, String Nome, List<Piatto> Piatti){
+    public Categoria(String Id, String Nome, ArrayList<Piatto> Piatti){
         this.Id = Id;
         this.Nome = Nome;
         this.Piatti = Piatti;
