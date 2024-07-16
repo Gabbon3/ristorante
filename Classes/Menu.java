30f3639d216b17f952edd1a3d5953aedd66fc76a
Heiji9 <andrea.carollo@edu.itspiemonte.it>
2024-07-16 10:23:38 +0200

    Classe Menu

---
+++ Classes/Menu.java
@@ -0,0 +1,23 @@
+package Classes;
+
+import java.util.List;
+
+public class Menu {
+    public List<String> Categoria;
+
+    public String getPiatto() {
+        return piatto;
+    }
+
+    public Menu (String Categoria) {
+        this.Categoria = categoria;
+    }
+
+}
+
+
+
+
+
+
+
