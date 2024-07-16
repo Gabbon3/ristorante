14d08a64d08a013fa006e8ce6f8a20fcba83f7ce
Gabriele <g2004gabbo@icloud.com>
2024-07-16 10:25:52 +0200

    Metodo Stampa Dettagli Aggiunto

--- Piatto.java
+++ Piatto.java
@@ -5,9 +5,17 @@ public class Piatto {
     public String Nome;
     public List<String> Ingredienti;
     public List<String> Allergeni;
-    public Integer TempoStimato;
+    public Integer TempoStimato; // tempo in minuti
     public Double Prezzo;
-
+    /**
+     * Costruttore del piatto con tutte le informazioni
+     * @param Id
+     * @param Nome
+     * @param Ingrediendi
+     * @param Allergeni
+     * @param TempoStimato
+     * @param Prezzo
+     */
     public Piatto(String Id, String Nome, List<String> Ingrediendi, List<String> Allergeni, Integer TempoStimato, Double Prezzo) {
         this.Id = Id;
         this.Nome = Nome;
@@ -16,6 +24,23 @@ public class Piatto {
         this.TempoStimato = TempoStimato;
         this.Prezzo = Prezzo;
     }
-
-    
+    /**
+     * Stampa i dettagli relativi al piatto
+     * @return dettagli
+     */
+    public String stampaDettagli() {
+        String dettagli = "Dettagli del piatto " + this.Nome + " (ID " + this.Id + ")";
+        // -- Ingredienti
+        dettagli += " - Ingredienti: ";
+        dettagli += String.join(", ", this.Ingredienti) + "\n";
+        // -- Allergeni
+        dettagli += " - Allergeni: ";
+        dettagli += String.join(", ", this.Allergeni) + "\n";
+        // -- Tempo stimato
+        dettagli += " - Tempo stimato: " + this.TempoStimato + " minuti\n";
+        // -- Prezzo
+        dettagli += " - Prezzo: " + this.Prezzo;
+        // ---
+        return dettagli;
+    }
 }
